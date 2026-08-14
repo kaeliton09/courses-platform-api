3b191a3f0b450f59d60817150f1b0094c082efcb
kaeliton09 <cardosokaeliton@gmail.,com>
2026-08-14 09:59:21 -0300

    feat: cria a entidade Categoria

--- src/main/java/dev/kaeliton/courses_platform_api/Models/CategoriaModel.java
+++ src/main/java/dev/kaeliton/courses_platform_api/Models/CategoriaModel.java
@@ -1,5 +1,27 @@
 package dev.kaeliton.courses_platform_api.Models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_categorias")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoriaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String nome;
+    private String descricao;
 
 }
