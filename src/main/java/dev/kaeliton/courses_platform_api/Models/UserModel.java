6568482b5746cc1c0fe15116098c872826a8a8bc
kaeliton09 <cardosokaeliton@gmail.,com>
2026-08-14 16:23:38 -0300

    feat: implementa a entidade UserModel

--- src/main/java/dev/kaeliton/courses_platform_api/Models/UserModel.java
+++ src/main/java/dev/kaeliton/courses_platform_api/Models/UserModel.java
@@ -1,5 +1,29 @@
 package dev.kaeliton.courses_platform_api.Models;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_usuarios")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    private String senha;
 
 }
