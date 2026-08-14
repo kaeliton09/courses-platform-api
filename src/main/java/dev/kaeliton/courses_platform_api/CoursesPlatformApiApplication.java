7810450c023cfd53e1e25b5a915c208e38bbe7ef
kaeliton09 <cardosokaeliton@gmail.,com>
2026-08-14 09:13:01 -0300

    first commit

---
+++ src/main/java/dev/kaeliton/courses_platform_api/CoursesPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package dev.kaeliton.courses_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kaeliton/courses_platform_api/CoursesPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kaeliton.courses_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
