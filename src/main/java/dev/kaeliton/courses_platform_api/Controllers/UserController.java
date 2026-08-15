70aaeef0c41dce8b25c18c333a8fca092c79bb5d
kaeliton09 <cardosokaeliton@gmail.,com>
2026-08-15 17:21:58 -0300

    feat: Implementa o userControler com as principais rotas do crud e cria o userService

--- src/main/java/dev/kaeliton/courses_platform_api/Controllers/UserController.java
+++ src/main/java/dev/kaeliton/courses_platform_api/Controllers/UserController.java
@@ -1,5 +1,55 @@
 package dev.kaeliton.courses_platform_api.Controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.kaeliton.courses_platform_api.DTOs.UserDTO;
+import dev.kaeliton.courses_platform_api.Services.UserService;
+
+
+
+
+@RestController
+@RequestMapping("/usuarios")
 public class UserController {
 
+
+    private UserService userService;
+    
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/listar")
+    public List<UserDTO> listarUsuarios() {
+        return userService.listarUsuarios();
+    }
+    
+    @GetMapping("/listar/{id}")
+    public UserDTO listarUsuarioPorId(@PathVariable Long id) {
+        return userService.listarUsuarioPorId(id);
+    }
+
+    @PostMapping("/criar")
+    public UserDTO criarUsuario(@RequestBody UserDTO userDTO) {
+        return userService.criarUsuario(userDTO);
+    }
+    
+    @DeleteMapping("/deletar/{id}")
+    public void deletarUsuario(@PathVariable Long id) {
+        userService.deletarUsuario(id);
+    }
+
+    @PutMapping("/alterar/{id}")
+    public UserDTO atualizarUsuario(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        return userService.atualizarUsuario(id, userDTO);
+    }
 }
--- src/main/java/dev/kaeliton/courses_platform_api/Services/UserService.java
+++ src/main/java/dev/kaeliton/courses_platform_api/Services/UserService.java
@@ -1,5 +1,66 @@
 package dev.kaeliton.courses_platform_api.Services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import dev.kaeliton.courses_platform_api.DTOs.UserDTO;
+import dev.kaeliton.courses_platform_api.Mappers.UserMappper;
+import dev.kaeliton.courses_platform_api.Models.UserModel;
+import dev.kaeliton.courses_platform_api.Repositorys.UserRepository;
+
+@Service
 public class UserService {
+
+
+    private final UserRepository userRepository;
+    private final UserMappper userMapper;
+
+    public UserService(UserRepository userRepository, UserMappper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+
+    //listar todos os usuários
+    public List<UserDTO> listarUsuarios() {
+        List<UserModel> usuarios = userRepository.findAll();
+        return usuarios.stream()
+                .map(userMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    //listar usuário por id
+    public UserDTO listarUsuarioPorId(Long id) {
+        UserModel usuario = userRepository.findById(id).orElse(null);
+        return usuario != null ? userMapper.toDTO(usuario) : null;
+    }
+
+    //criar usuário
+    public UserDTO criarUsuario(UserDTO userDTO) {
+        UserModel userModel = userMapper.toModel(userDTO);
+        UserModel usuarioSalvo = userRepository.save(userModel);
+        return userMapper.toDTO(usuarioSalvo);
+    }
+
+    //deletar usuário
+    public void deletarUsuario(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    //atualizar usuário
+    public UserDTO atualizarUsuario(Long id, UserDTO userDTO) {
+        UserModel usuarioExistente = userRepository.findById(id).orElse(null);
+        if (usuarioExistente != null) {
+            usuarioExistente.setId(id);
+            usuarioExistente.setNome(userDTO.getNome());
+            usuarioExistente.setEmail(userDTO.getEmail());
+            usuarioExistente.setSenha(userDTO.getSenha());
+            UserModel usuarioAtualizado = userRepository.save(usuarioExistente);
+            return userMapper.toDTO(usuarioAtualizado);
+        }
+        return null;
+    }
 
 }
