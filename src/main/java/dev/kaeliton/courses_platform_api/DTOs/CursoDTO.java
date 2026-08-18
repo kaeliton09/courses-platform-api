30a1ecd612e88da4986b284343fdab030544e61b
kaeliton09 <cardosokaeliton@gmail.,com>
2026-08-18 09:32:19 -0300

    feat: cria e implementa o CursoDTO para tirar a responsabilidade comunicacao entre o banco de dados e o model

--- src/main/java/dev/kaeliton/courses_platform_api/DTOs/CursoDTO.java
+++ src/main/java/dev/kaeliton/courses_platform_api/DTOs/CursoDTO.java
@@ -1,4 +1,20 @@
 package dev.kaeliton.courses_platform_api.DTOs;
 
+import dev.kaeliton.courses_platform_api.Models.NivelCurso;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CursoDTO {
+
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private NivelCurso nivel;
+    private int totalAulas;
+    private int totalHoras;
+
 }
--- src/main/java/dev/kaeliton/courses_platform_api/Mappers/CursoMapper.java
+++ src/main/java/dev/kaeliton/courses_platform_api/Mappers/CursoMapper.java
@@ -1,4 +1,36 @@
 package dev.kaeliton.courses_platform_api.Mappers;
 
+import dev.kaeliton.courses_platform_api.DTOs.CursoDTO;
+import dev.kaeliton.courses_platform_api.Models.CursoModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CursoMapper {
+
+    public CursoDTO toDTO(CursoModel cursoModel){
+
+        CursoDTO cursoDTO = new CursoDTO();
+        cursoDTO.setId(cursoModel.getId());
+        cursoDTO.setTitulo(cursoModel.getTitulo());
+        cursoDTO.setDescricao(cursoModel.getDescricao());
+        cursoDTO.setNivel(cursoModel.getNivel());
+        cursoDTO.setTotalAulas(cursoModel.getTotalAulas());
+        cursoDTO.setTotalHoras(cursoModel.getTotalHoras());
+
+        return cursoDTO;
+    }
+
+    public CursoModel toModel(CursoDTO cursoDTO){
+
+        CursoModel cursoModel = new CursoModel();
+
+        cursoModel.setId(cursoDTO.getId());
+        cursoModel.setTitulo(cursoDTO.getTitulo());
+        cursoModel.setDescricao(cursoDTO.getDescricao());
+        cursoModel.setNivel(cursoDTO.getNivel());
+        cursoModel.setTotalAulas(cursoDTO.getTotalAulas());
+        cursoModel.setTotalHoras(cursoDTO.getTotalHoras());
+
+        return cursoModel;
+    }
 }
